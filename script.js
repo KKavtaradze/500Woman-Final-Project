84092d9ca872a1da623547a275ff78b1fc182f9e
KKavtaradze <116086699+KKavtaradze@users.noreply.github.com>
2022-11-30 16:06:51 +0100

    test

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+let burgerBar = document.getElementById("burgerBar");
+let navigation = document.getElementById("navigation");
+burgerBar.addEventListener("click", function () {
+  navigation.classList.toggle("navDrop");
+  burgerBar.classList.toggle("barMove");
+});
+
+let bannerNone = document.getElementById("bannerNone");
+burgerBar.addEventListener("click", function () {
+  bannerNone.classList.toggle("banner-wrapper-none");
+});
